b6b6781ad6ea523fe517706f2efd613d626d8f0f
Logan-Krumholz <122186100+Logan-Krumholz@users.noreply.github.com>
2023-05-09 12:58:01 -0700

    Create tnine.c

---
+++ tnine.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "trienode.h"
+
+// run_session run the on-going decoding interaction with the
+// user.  It requires a previously build wordTrie to work.
+void run_session(trieNode *wordTrie);
+
+// build_trie builds a trie using the words from dictionary.
+// Returns the root node of the trie.
+trieNode* build_trie(FILE *dictionary) {
+  trieNode *root = create_node();
+  char word[MAX_WORD_LEN];
+
+  while (fgets(word, MAX_WORD_LEN, dictionary)) {
+    insert_word(root, word);
+  }
+
+  return root;
+}
+
+int main(int argc, char **argv) {
+  FILE *dictionary = NULL;
+  trieNode *wordTrie = NULL;
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s [DICTIONARY]\n", argv[0]);
+    return EXIT_FAILURE;
+  } else {
+    dictionary = fopen(argv[1], "r");
+    if (!dictionary) {
+      fprintf(stderr, "Error: Cannot open %s\n", argv[1]);
+      return EXIT_FAILURE;
+    }
+  }
+
+  // build the trie
+  wordTrie = build_trie(dictionary);
+
+  // run interactive session
+  run_session(wordTrie);
+
+  // clean up
+  delete_trie(wordTrie);
+  fclose(dictionary);
+
+  return EXIT_SUCCESS;
+}
