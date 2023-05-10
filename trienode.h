298f69ab33d5909e00a98ce7fa6280d873389169
Logan-Krumholz <122186100+Logan-Krumholz@users.noreply.github.com>
2023-05-10 14:08:31 -0700

    Update trienode.h

--- trienode.h
+++ trienode.h
@@ -1,7 +1,7 @@
-
- /* trienode.h contains structure and function declarations for 
-   a trie implementation 
-   CSE374, HW5, Logan Krumholz 
+/* trienode.h contains structure and function declarations for 
+ * a trie implementation
+ * CSE374, HW5, Logan Krumholz 
+ * 5/10/23
 */
 
 #ifndef TRIENODE_H
@@ -10,7 +10,7 @@
 #include <stdio.h>
 
 #define MAXLEN 50
-#define BRANCHES 11
+#define BRANCHES 9
 
 // trieNode is one node in an 9-tree, where branches correspond to
 // # and the T9 alphabet.
@@ -19,8 +19,7 @@ typedef struct trieNode {
   char *word;
 } trieNode;
 
-// given a word list (dict), build a trie that holds
-// each word.  Return pointer to the root node.
+// Initializes a trie tree 
 trieNode * build_tree();
 
 // given a pattern, return the word stored in the
@@ -29,7 +28,7 @@ char* get_word(trieNode *root, char *pattern);
 int T9conversion(char letter);
 void build_trie(trieNode * root, char * s);
 trieNode * find_nodes(trieNode * root, char * number);
-// deletes the entire tree branching from root
+// Cleans up memory 
 void malfree(trieNode *root);
 
 
