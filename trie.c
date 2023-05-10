7487238e1005896e342d6cfee38d4b6ab9fca7c4
Logan-Krumholz <122186100+Logan-Krumholz@users.noreply.github.com>
2023-05-10 14:08:07 -0700

    Update trie.c

--- trie.c
+++ trie.c
@@ -1,5 +1,5 @@
 /*
- * trie.c - Creates tries and is resposnsible for building, traversing,
+ * trie.c - Creates tries and is responsible for building, traversing,
  * and adding to existing Tries. Additionally the logic for T9 conversion is added as well.
  * CSE 374 HW 5 Logan Krumholz
  * 5/10/23
@@ -13,11 +13,10 @@
 
 // Create empty trie 
 trieNode * build_tree() {
-  trieNode * node = (trieNode *) malloc(sizeof(trieNode));
-  node -> word = NULL;
-  int i;
-  for (i = 0; i < 11; i++) {
-    node -> branches[i] = NULL;
+  trieNode * node = malloc(sizeof(trieNode));
+  node->word = NULL;
+  for (int i = 0; i < 11; i++) {
+    node->branches[i] = NULL;
   }
   return node;
 }
@@ -41,63 +40,59 @@ int T9conversion(char letter) {
 void build_trie(trieNode * root, char * s) {
   trieNode * current = root;
   int length = strlen(s);
-  char * text = (char *) malloc(length);
+  char * text = malloc(length);
   if (text != NULL) {
     strncpy(text, s, length - 1);
     text[length - 1] = '\0';
   }
-  int i = 0;
-  while (s[i] != '\n') {
+  for (int i = 0; i < length - 1; i++) {
     int digit = T9conversion(s[i]);
     if (digit == 0) {
       // Remove excess chars not alphabet
-      i++;
       continue;
     }
-    if (current -> branches[digit] == NULL) {
-      current -> branches[digit] = build_tree();
+    if (current->branches[digit] == NULL) {
+      current->branches[digit] = build_tree();
     }
-    current = current -> branches[digit];
-    i++;
+    current = current->branches[digit];
   }
-  while (current -> branches[10] != NULL) {
-    current = current -> branches[10];
+  while (current->branches[10] != NULL) {
+    current = current->branches[10];
   }
-  if (current -> word == NULL) {
-    current -> word = text;
+  if (current->word == NULL) {
+    current->word = text;
   } else {
-    current -> branches[10] = build_tree();
-    current = current -> branches[10];
-    current -> word = text;
+    current->branches[10] = build_tree();
+    current = current->branches[10];
+    current->word = text;
   }
 }
 
 // Traverse Trie given word
 trieNode * find_nodes(trieNode * root, char * number) {
   trieNode * cur = root;
-  int i;
-  for (i = 0; i < strlen(number); i++) {
+  for (int i = 0; i < strlen(number); i++) {
     if (number[i] != '#') {
       int digit = number[i] - '0';
       if (digit < 2 || digit > 9) {
-        // ignore invalid digits
+        // Ignore other inputs
         continue;
       }
-      if (cur -> branches[digit] == NULL) {
+      if (cur->branches[digit] == NULL) {
         return NULL;
       }
-      cur = cur -> branches[digit];
+      cur = cur->branches[digit];
     } else {
-      if (cur -> branches[10] == NULL) {
+      if (cur->branches[10] == NULL) {
         return NULL;
       }
-      cur = cur -> branches[10];
+      cur = cur->branches[10];
     }
   }
   return cur;
 }
 
-// Recursively disconnect by freeing
+// Recursively disconnect by freeing memory
 void malfree(trieNode * root) {
   int i;
   for (i = 0; i < 11; i++) {
